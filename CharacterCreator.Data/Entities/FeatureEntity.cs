c14d5964881e60d0dc9b23da0e301e927bb2733d
Rigo-rios <146138438+Rigo-rios@users.noreply.github.com>
2024-01-18 17:46:11 -0500

    Added Props to Entities

--- CharacterCreator.Data/Entities/FeatureEntity.cs
+++ CharacterCreator.Data/Entities/FeatureEntity.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,18 @@ namespace CharacterCreator.Data.Entities
 {
     public class FeatureEntity
     {
+        [Key]
+        public int FeatureId { get; set; }
+        public string EyeColor { get; set; }= string.Empty;
+        public string HairStyle { get; set; }= string.Empty;
+        public string HairColor { get; set; }= string.Empty;
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public string BodyType { get; set; } = string.Empty;
+        public string Ability { get; set; } = string.Empty;
+        public string SkinColor { get; set; } = string.Empty;
+        [ForeignKey(nameof(CharacterId))]
+        public int CharacterId { get; set; }
         
     }
 }
