8682e5160dd4a397129f0f42a51c6eca95d8bc5b
ramontgomery <ryan.andrew.montgomery@gmail.com>
2024-01-24 18:55:34 -0500

    removed using

--- CharacterCreator.Models/Models/TeamModels/TeamList.cs
+++ CharacterCreator.Models/Models/TeamModels/TeamList.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CharacterCreator.Models.Models.CharacterModels;
-using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
 
 namespace CharacterCreator.Models.Models.TeamModels
 {
